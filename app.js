20d2cbafe4b5da8e51383de660b9e4092fca22c7
Subhasis Das <subhasisdas125@gmail.com>
2020-01-04 01:07:17 +0530

    here we go again

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyparser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config({ path: "../example.env" });
+
+const app = express();
+
+app.use(cors());
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
+
+const server = app.listen(3000, () => console.log("server running"));
+
+app.get("/", (req, res) => {
+  res.send(
+    "Server working on  " +
+      server.address().address +
+      ` env variable =  ${process.env.EXAMPLE}`
+  );
+});
